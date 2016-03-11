af2e6bb9b804b22da12bb9c819d96de9d7da5f2b
feiin <feiin@qq.com>
2016-03-11 13:46:33 +0800

    fix

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var jssdk = require('./lib/jssdk');
 
 function getJssdkConfig(options,callback) {
 
-    jssdk.getJsApiTicket(options.appConfig,function(err,ticket) {
+    jssdk.getJsApiTicket(options,function(err,ticket) {
         if(err) {
             return callback(err);
         }
         var result = jssdk.createSha1Sign(ticket, options.url);
-        result.appId = options.appConfig.appId;
+        result.appId = options.appId;
         callback(null, result);
     });
 }
